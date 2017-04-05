67ab9cc6aef383510b0a70aef2255855a02954c6
Joshua Evans <skyfriends1@gmail.com>
2017-04-05 16:12:49 -0700

    added 6th question, fixed some style issues, working on 7th question

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'user strict';
 
-alert('Welcome to my About Me page!  Lets play a guessing game so you can get to know me bette.')
+var userName = prompt('My name is Joshua, what is your name?');
+alert('Nice to meet you, ' + userName + '. Welcome to my About Me page!  Lets play a guessing game so you can get to know me better.');
 
 var dogs;
 var fruit;
@@ -8,13 +9,12 @@ var paint;
 var chickens;
 var records;
 
-
 dogs = prompt('Do I like dogs?');
 
 if(dogs.trim().toLowerCase() == 'yes'){
-  alert('Youre right, I like dogs.  I dont have one right now, but hopefully soon');
+  alert('You\'re right, I like dogs.  I don\'t have one right now, but hopefully I will soon');
 } else {
-  alert('Who doesnt love dogs?');
+  alert('What!? Who doesnt love dogs?');
 }
 
 console.log('The answer given for the dogs question was', dogs);
@@ -22,9 +22,9 @@ console.log('The answer given for the dogs question was', dogs);
 fruit = prompt('Is my favorite fruit apples?');
 
 if(fruit.trim().toLowerCase() == 'yes'){
-  alert('Actually, im not really a huge fan');
+  alert('Actually, i\'m not really a huge fan of apples');
 } else {
-  alert('Youre right, im more of a citrus lover');
+  alert('You\'re right, im more of a citrus lover');
 }
 
 console.log('The answer given for the fruit question was', fruit);
@@ -56,3 +56,45 @@ if(records.trim().toLowerCase() == 'yes') {
   alert('Incorrect!  Collecting and selling vinyl has been my line of work for a long time!')
 }
 console.log('The answer given for the records question was', records);
+
+
+
+
+
+alert('Now, try to guess the day of the month of my birthday!')
+
+var actualBirthday = 3;
+var completedDayGame = false
+var userGuessDay;
+
+
+for (userTrying = 0; userTrying < 4 && completedDayGame == false; userTrying++) {
+  userGuessDay = prompt('Take a Guess!');
+  console.log('The number that the user guessed was', userGuessDay);
+
+  if (userGuessDay == 3) {
+    completedGame = true;
+    alert('That\'s right! My birthday is May 3rd.');
+  } else if (userGuessDay < 3) {
+    alert('You\'re close, but you have to guess a slightly higher number.');
+  } else {
+    alert('Nope! Guess again')
+  }
+}
+
+alert('I have lived in a lot of different parts of Seattle.  Can you guess the neighborhoods I have lived in?')
+
+var livedHoods = ['ballard', 'downtown', 'belltown', 'west seattle'];
+var count = 0
+
+for(userTrying = 0; userTrying < 6; userTrying++) {
+  userGuessHood = prompt('Take a guess!');
+  console.log('The neighborhood guessed by the user was', userGuessHood);
+
+  if (livedHoods.includes(userGuessHood)) {
+    alert('You got one right!')
+    count = count + 1
+  }
+}
+
+alert('You got ' + count + ' out of 4 correct!\nHere are all four:\n\tBallard\n\tBelltown\n\tWest Seattle\n\tDowntown')
