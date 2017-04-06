6ba2b12cdf22bae63b5eae6ca535a4469e8baea9
Joshua Evans <skyfriends1@gmail.com>
2017-04-06 13:18:31 -0700

    updated last questions

--- app.js
+++ app.js
@@ -8,25 +8,26 @@ var fruit;
 var paint;
 var chickens;
 var records;
+
 dogs = prompt('Do I like dogs?');
 if (dogs.trim().toLowerCase() == 'yes') {
     alert('You\'re right, I like dogs.  I don\'t have one right now, but hopefully I will soon');
 } else {
-    alert('What!? Who doesnt love dogs?');
+    alert('What!? Who doesn\'t love dogs?');
 }
 console.log('The answer given for the dogs question was', dogs);
 fruit = prompt('Is my favorite fruit apples?');
 if (fruit.trim().toLowerCase() == 'yes') {
-    alert('Actually, i\'m not really a huge fan of apples');
+    alert('Actually, I\'m not really a huge fan of apples');
 } else {
-    alert('You\'re right, im more of a citrus lover');
+    alert('You\'re right, I\'m prefer oranges.');
 }
 console.log('The answer given for the fruit question was', fruit);
 paint = prompt('Do I like to paint?');
 if (paint.trim().toLowerCase() == 'yes') {
     alert('Nah, I spend most of my time coding or playing music');
 } else {
-    alert('Youre right!');
+    alert('You\'re right!');
 }
 console.log('The answer given for the paint question was', paint);
 chickens = prompt('Do I have chickens living in my yard?');
@@ -44,11 +45,12 @@ if (records.trim().toLowerCase() == 'yes') {
 }
 console.log('The answer given for the records question was', records);
 alert('Now, try to guess the day of the month of my birthday!');
+
 var actualBirthday = 3;
 var completedDayGame = false;
 var userGuessDay;
-var userTrying;
-for (userTrying = 0; userTrying < 4 && completedDayGame == false; userTrying++) {
+var userTry;
+for (userTry = 0; userTry < 4 && completedDayGame == false; userTry++) {
     userGuessDay = prompt('Take a Guess!');
     console.log('The number that the user guessed was', userGuessDay);
     if (userGuessDay == 3) {
@@ -67,14 +69,14 @@ var livedHoods = ['ballard', 'downtown', 'belltown', 'west seattle'];
 var count = 0;
 var userGuessHood;
 
-for (userTrying = 0; userTrying < 6; userTrying++) {
+for (userTry = 0; userTry < 6; userTry++) {
     userGuessHood = prompt('Take a guess!');
     console.log('The neighborhood guessed by the user was ' + userGuessHood);
     if (livedHoods.includes(userGuessHood)) {
-        alert('You got one right!')
-        count = count + 1
+        alert('You got one right!');
+        count++;
     }
 }
 
-alert('You got ' + count + ' out of 4 correct!\nHere are all four:\n\tBallard\n\tBelltown\n\tWest Seattle\n\tDowntown')
+alert('Good job, ' + userName + ' You got ' + count + ' out of 4 correct!\nHere are all four:\n\tBallard\n\tBelltown\n\tWest Seattle\n\tDowntown');
 console.log('The total correct answers for the neighborhood game was ' + count);
